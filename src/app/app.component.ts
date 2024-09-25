b71a25f5eccee7b706206049cb29fddd2d03e3f0
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-09-25 13:12:10 +0200

    component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer/footer.component';
+import { ProjectComponent } from './component/project/project/project.component';
+import { PresentationComponent } from './component/presentation/presentation/presentation.component';
+import { SkillsComponent } from './component/skills/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ProjectComponent, PresentationComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
