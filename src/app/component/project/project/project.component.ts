298a6c166c15fda6ef33cf256777b9dfdb0ab7db
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-10-02 18:24:27 +0200

    project section structure and minimal css

--- src/app/component/project/project/project.component.ts
+++ src/app/component/project/project/project.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ProjectcardComponent } from '../../projectcard/projectcard/projectcard.component';
+import { Project } from '../../../models/project.model';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [ProjectcardComponent],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
+  projects: Project[] = [
+    {
+      name: "Did you beat it?",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 
+      image: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg",
+      githubLink: "https://github.com/WildCodeSchool/did-you-beat-it"
+    },
+    {
+      name: "Anciens meubles pour une nouvelle vie",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 
+      image: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg",
+      githubLink: "https://github.com/clotildebeckerlamoureux/PC-anciens-meubles-pour-une-nouvelle-vie-FRONT"
+    },
+    {
+      name: "Ghibli movies",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 
+      image: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg",
+      githubLink: "https://github.com/clotildebeckerlamoureux/Projet-collectif-Dataviz-OJC"
+    }
+
+  ]
 
 }
--- src/app/component/projectcard/projectcard/projectcard.component.ts
+++ src/app/component/projectcard/projectcard/projectcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-projectcard',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './projectcard.component.css'
 })
 export class ProjectcardComponent {
-
+  @Input() projectName: string = "";
+  @Input() projectDescription: string = "";
+  @Input() projectImage: string = "";
+  @Input() projectLink: string = "";
 }
